0729259ec60e35c70e50fce663461de37a2d52f5
Joseeustaquio <taquinhomz@gmail.com>
2023-06-03 22:08:15 -0300

    Instanciando a classe fornecedor com seus atributos

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,6 +21,13 @@ public class HomeController : Controller
         Cliente cliente3 = new Cliente(3,"João Paulo","jp@yahoo","33333333333", "Touche");
         Cliente cliente4 = new Cliente(4,"Madalena Silva", "44444444444","ms@teste.com", "Pernalonga");
         Cliente cliente5 = new Cliente(5,"Poncio Pilatos","55555555555", "pp@teste.com", "piu piu");
+
+       //Instanciando a classe fornecedor
+        Fornecedor fornecedor1 = new Fornecedor(1,"Pet.SA","11111110001.66", "jm@teste.com");
+        Fornecedor fornecedor2 = new Fornecedor(2,"Mundo dog","222220001/77","mj@gmail.com");
+        Fornecedor fornecedor3 = new Fornecedor(3,"Clinica Animal","333333/0001.88", "ca@teste.com");
+        Fornecedor fornecedor4 = new Fornecedor(4,"Socor Bichos", "4444444/0001.99","sb@teste.com");
+        Fornecedor fornecedor5 = new Fornecedor(5,"Veterinaria pet","55555/0001.11", "vp@teste.com");
         return View();
     }
 
