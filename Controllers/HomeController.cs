61dde0b4b6feb086c2cf9028b28f8ecf81983f3b
Joseeustaquio <taquinhomz@gmail.com>
2023-06-03 21:46:38 -0300

    Instaciando a classe cliente dentro de controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,12 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        //instanciando a classe cliente
+        Cliente cliente1 = new Cliente(1,"José Maria","11111111111", "jm@teste.com","totó");
+        Cliente cliente2 = new Cliente(2,"Maria José","22222222222","mj@gmail.com","bichano");
+        Cliente cliente3 = new Cliente(3,"João Paulo","jp@yahoo","33333333333", "Touche");
+        Cliente cliente4 = new Cliente(4,"Madalena Silva", "44444444444","ms@teste.com", "Pernalonga");
+        Cliente cliente5 = new Cliente(5,"Poncio Pilatos","55555555555", "pp@teste.com", "piu piu");
         return View();
     }
 
