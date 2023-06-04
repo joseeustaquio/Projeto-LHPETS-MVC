e657a6ba725faf1fe6280eb10b8e5a248e222340
Joseeustaquio <taquinhomz@gmail.com>
2023-06-03 21:06:39 -0300

    Criando a classe cliente com seus atributos

---
+++ Models/Cliente.cs
@@ -0,0 +1,11 @@
+namespace LHPET.Models;
+
+public class Cliente
+{
+    //criando a clase cliente com seus atributos
+    public int Id{get; set;}
+    public string Nome {get; set;}
+    public string Cpf {get; set;}
+    public string Email {get; set;}
+    public string Paciente {get; set;}
+}
