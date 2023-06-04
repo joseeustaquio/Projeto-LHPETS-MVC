30fb02a5d8bbe2b922a397b23a70d3a608b49bbd
Joseeustaquio <taquinhomz@gmail.com>
2023-06-03 21:14:12 -0300

    Criando o método construtor com parametro

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -8,4 +8,14 @@ public class Cliente
     public string Cpf {get; set;}
     public string Email {get; set;}
     public string Paciente {get; set;}
+
+    //Criando o método construtor com parâmetros
+    public Cliente (int id, string nome, string cpf, string email, string paciente)
+    {
+        this.Id = id;
+        this.Nome = nome;
+        this.Cpf = cpf;
+        this.Email = email;
+        this.Paciente = paciente;
+    }
 }
